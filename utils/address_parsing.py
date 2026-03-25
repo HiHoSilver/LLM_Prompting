e3575aeeb4ee7572a948877fcb1122bb8bc49ca6
Nick Silver <nsilver@associates.nsf.gov>
2026-03-25 10:11:25 -0400

    updates

--- utils/address_parsing.py
+++ utils/address_parsing.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 def parse_addresses(df: pd.DataFrame) -> pd.DataFrame:
+    if df is None or df.empty:
+        print("Input file is empty or invalid")
+        return 
+    
     split_data = df['response'].str.split('; ', expand=True)
     df[['street', 'city', 'state', 'zip']] = split_data
     return df
